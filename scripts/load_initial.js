7550214d787137b2b9fa8790e537181d3a6e83e5
Ivo Paounov <ivo.paunov@gmail.com>
2015-07-23 15:13:05 +0300

    CSS touches, font embedding test

--- scripts/load_initial.js
+++ scripts/load_initial.js
@@ -1,6 +1,7 @@
 (function (initIntroAnimation) {
     var bodyElement = document.getElementsByTagName('body')[0],
         introCanvas = document.createElement('canvas'),
+        wrapper = document.createElement('div'),
         documentFragment = document.createDocumentFragment(),
         CONSTANTS = {
             INTRO_CANVAS_WIDTH: '800',
@@ -13,12 +14,23 @@
         introCanvas.setAttribute('height', CONSTANTS.INTRO_CANVAS_HEIGHT);
     }
 
-    function appendToBody(){
-        documentFragment.appendChild(introCanvas);
+    function initWrapperDiv() {
+        wrapper.setAttribute('id', 'wrapper');
+        wrapper.appendChild(introCanvas);
+        bodyElement.appendChild(wrapper);
+    }
+
+    function appendToFragment() {
+        documentFragment.appendChild(wrapper);
+    }
+
+    function appendToBody() {
         bodyElement.appendChild(documentFragment);
     }
 
     initIntroCanvas();
+    initWrapperDiv();
+    appendToFragment();
     appendToBody();
 
     bodyElement.onload = initIntroAnimation(introCanvas);
