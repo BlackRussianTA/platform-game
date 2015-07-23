11f6c80421bd0c4e93fb24f24040e3c04d687687
Ivo Paounov <ivo.paunov@gmail.com>
2015-07-23 14:11:05 +0300

    Add initIntroAnimation as separate js file - working

--- scripts/load_initial.js
+++ scripts/load_initial.js
@@ -1,26 +1,3 @@
-// TODO: To extract as separate js file after RequireJS usage conformation
-
-var initIntroAnimation  = (function (canvas) {
-    function initIntroAnimation (canvas)  {
-        var stage,
-            exportRoot;
-
-        (function () {
-            canvas = document.getElementById("intro-animation-canvas");
-            exportRoot = new lib.BlackRussianIntro();
-
-            stage = new createjs.Stage(canvas);
-            stage.addChild(exportRoot);
-            stage.update();
-
-            createjs.Ticker.setFPS(lib.properties.fps);
-            createjs.Ticker.addEventListener("tick", stage);
-        } ())
-    }
-
-    return initIntroAnimation;
-}());
-
 (function (initIntroAnimation) {
     var bodyElement = document.getElementsByTagName('body')[0],
         introCanvas = document.createElement('canvas'),
