69cc2bd68d44f9ff7985f9107f6efcbf33f68a30
Ivo Paounov <ivo.paunov@gmail.com>
2015-07-26 12:46:42 +0300

    Correcting animations, and add ides player animations

--- character_and_bg_movement/spriteFramesAndAnimationSequence.js
+++ character_and_bg_movement/spriteFramesAndAnimationSequence.js
@@ -119,6 +119,28 @@ var frames = {
 };
 
 var animationPlayer = {
+        idleForward: [
+            frames.playerWalkFront002.frame.x,
+            frames.playerWalkFront002.frame.y,
+            frames.playerWalkFront002.frame.w,
+            frames.playerWalkFront002.frame.h,
+
+            frames.playerWalkFront006.frame.x,
+            frames.playerWalkFront006.frame.y,
+            frames.playerWalkFront006.frame.w,
+            frames.playerWalkFront006.frame.h
+        ],
+        idleBackward: [
+            frames.playerWalkBack002.frame.x,
+            frames.playerWalkBack002.frame.y,
+            frames.playerWalkBack002.frame.w,
+            frames.playerWalkBack002.frame.h,
+
+            frames.playerWalkBack006.frame.x,
+            frames.playerWalkBack006.frame.y,
+            frames.playerWalkBack006.frame.w,
+            frames.playerWalkBack006.frame.h
+        ],
         walkForward: [
             frames.playerWalkFront001.frame.x,
             frames.playerWalkFront001.frame.y,
@@ -129,17 +151,16 @@ var animationPlayer = {
             frames.playerWalkFront002.frame.y,
             frames.playerWalkFront002.frame.w,
             frames.playerWalkFront002.frame.h,
-            frames.playerWalkFront002.frame.x,
 
+            frames.playerWalkFront003.frame.x,
             frames.playerWalkFront003.frame.y,
             frames.playerWalkFront003.frame.w,
             frames.playerWalkFront003.frame.h,
-            frames.playerWalkFront003.frame.x,
 
+            frames.playerWalkFront004.frame.x,
             frames.playerWalkFront004.frame.y,
             frames.playerWalkFront004.frame.w,
             frames.playerWalkFront004.frame.h,
-            frames.playerWalkFront004.frame.x,
 
             frames.playerWalkFront005.frame.x,
             frames.playerWalkFront005.frame.y,
@@ -161,17 +182,16 @@ var animationPlayer = {
             frames.playerWalkBack002.frame.y,
             frames.playerWalkBack002.frame.w,
             frames.playerWalkBack002.frame.h,
-            frames.playerWalkBack002.frame.x,
 
+            frames.playerWalkBack003.frame.x,
             frames.playerWalkBack003.frame.y,
             frames.playerWalkBack003.frame.w,
             frames.playerWalkBack003.frame.h,
-            frames.playerWalkBack003.frame.x,
 
+            frames.playerWalkBack004.frame.x,
             frames.playerWalkBack004.frame.y,
             frames.playerWalkBack004.frame.w,
             frames.playerWalkBack004.frame.h,
-            frames.playerWalkBack004.frame.x,
 
             frames.playerWalkBack005.frame.x,
             frames.playerWalkBack005.frame.y,
@@ -193,17 +213,16 @@ var animationPlayer = {
             frames.playerJumpFront002.frame.y,
             frames.playerJumpFront002.frame.w,
             frames.playerJumpFront002.frame.h,
-            frames.playerJumpFront002.frame.x,
 
+            frames.playerJumpFront003.frame.x,
             frames.playerJumpFront003.frame.y,
             frames.playerJumpFront003.frame.w,
             frames.playerJumpFront003.frame.h,
-            frames.playerJumpFront003.frame.x,
 
+            frames.playerJumpFront004.frame.x,
             frames.playerJumpFront004.frame.y,
             frames.playerJumpFront004.frame.w,
             frames.playerJumpFront004.frame.h,
-            frames.playerJumpFront004.frame.x,
 
             frames.playerJumpFront005.frame.x,
             frames.playerJumpFront005.frame.y,
@@ -225,12 +244,11 @@ var animationPlayer = {
             frames.playerJumpBack002.frame.y,
             frames.playerJumpBack002.frame.w,
             frames.playerJumpBack002.frame.h,
-            frames.playerJumpBack002.frame.x,
 
+            frames.playerJumpBack003.frame.x,
             frames.playerJumpBack003.frame.y,
             frames.playerJumpBack003.frame.w,
             frames.playerJumpBack003.frame.h,
-            frames.playerJumpBack003.frame.x,
 
             frames.playerJumpBack004.frame.y,
             frames.playerJumpBack004.frame.w,
@@ -259,12 +277,11 @@ var animationPlayer = {
             frames.enemy1Move002.frame.y,
             frames.enemy1Move002.frame.w,
             frames.enemy1Move002.frame.h,
-            frames.enemy1Move002.frame.x,
 
+            frames.enemy1Move003.frame.x,
             frames.enemy1Move003.frame.y,
             frames.enemy1Move003.frame.w,
             frames.enemy1Move003.frame.h,
-            frames.enemy1Move003.frame.x,
 
             frames.enemy1Move004.frame.y,
             frames.enemy1Move004.frame.w,
@@ -283,12 +300,11 @@ var animationPlayer = {
             frames.enemy2Move002.frame.y,
             frames.enemy2Move002.frame.w,
             frames.enemy2Move002.frame.h,
-            frames.enemy2Move002.frame.x,
 
+            frames.enemy2Move003.frame.x,
             frames.enemy2Move003.frame.y,
             frames.enemy2Move003.frame.w,
             frames.enemy2Move003.frame.h,
-            frames.enemy2Move003.frame.x,
 
             frames.enemy2Move004.frame.y,
             frames.enemy2Move004.frame.w,
